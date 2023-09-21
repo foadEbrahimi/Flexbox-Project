7b89679f9ba5b427bbd362b00c9e8b352bb08957
Foad <fwad40901@gmail.com>
2023-09-21 12:48:09 +0330

    initial project

---
+++ README.md
@@ -0,0 +1,4 @@
+# flexbox-project
+
+### this is a project with flexbox
+You Can see the demo of this project with this link https://foadebrahimi.github.io/flexbox-project/
---
+++ js/app.js
@@ -0,0 +1,27 @@
+// nav toggle
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click" , () => {
+    links.classList.toggle("show-links")
+});
+
+//smooth scroll
+//select links
+const scrolllinks = document.querySelectorAll(".scroll-link");
+scrolllinks.forEach(link => {
+    link.addEventListener("click" , e => {
+        e.preventDefault();
+        links.classList.remove("show-links");
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        let position = element.offsetTop - 62;
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: "smooth"
+        });
+    })
+})
